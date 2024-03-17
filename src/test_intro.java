72e78a37d60410c2a69d7055735c6f224563cfa3
sonujgm <souvikde096@gmail.com>
2024-03-17 19:35:08 +0530

    test

---
+++ src/test_intro.java
@@ -0,0 +1,17 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class test_intro {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.setProperty("webDriver.chrome.driver", "C:\\Users\\DELL\\drivers\\chromedriver.exe");
+		WebDriver driver= new ChromeDriver();
+		driver.get("https://account.box.com/login"); 
+		System.out.println(driver.getTitle());
+		driver.quit();
+		
+	}
+
+}
